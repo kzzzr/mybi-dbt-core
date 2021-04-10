1dd3e2727d47f62cf684dbefc8cce395cf906845
Artemiy Kozyr <a.kozyr@wheely.com>
2021-04-11 02:04:58 +0300

    qualify macro reference with package name

--- macros/surrogate_key.sql
+++ macros/surrogate_key.sql
@@ -2,7 +2,7 @@
 --- MSSQL hash macro ---
 ------------------------
 {% macro hash(field) -%}
-  HASHBYTES('SHA2_256', {{field}})
+    HASHBYTES('SHA2_256', {{field}})
 {%- endmacro %}
 
 --------------------------
@@ -18,42 +18,41 @@
 --------------------------
 {%- macro surrogate_key(field_list) -%}
 
+    {%- if varargs|length >= 1 %}
 
-{%- if varargs|length >= 1 %}
+    {%- do exceptions.warn("Warning: the `surrogate_key` macro now takes a single list argument instead of multiple string arguments. Support for multiple string arguments will be deprecated in a future release of dbt-utils.") -%}
 
-{%- do exceptions.warn("Warning: the `surrogate_key` macro now takes a single list argument instead of multiple string arguments. Support for multiple string arguments will be deprecated in a future release of dbt-utils.") -%}
+    {# first argument is not included in varargs, so add first element to field_list_xf #}
+    {%- set field_list_xf = [field_list] -%}
 
-{# first argument is not included in varargs, so add first element to field_list_xf #}
-{%- set field_list_xf = [field_list] -%}
+    {%- for field in varargs %}
+    {%- set _ = field_list_xf.append(field) -%}
+    {%- endfor -%}
 
-{%- for field in varargs %}
-{%- set _ = field_list_xf.append(field) -%}
-{%- endfor -%}
+    {%- else -%}
 
-{%- else -%}
+    {# if using list, just set field_list_xf as field_list #}
+    {%- set field_list_xf = field_list -%}
 
-{# if using list, just set field_list_xf as field_list #}
-{%- set field_list_xf = field_list -%}
-
-{%- endif -%}
-
-
-{%- set fields = [] -%}
-
-{%- for field in field_list_xf -%}
-
-    {%- set _ = fields.append(
-        "coalesce(cast(" ~ field ~ " as NVARCHAR " ~ "), '')"
-    ) -%}
-
-    {%- if not loop.last %}
-        {%- set _ = fields.append("'-'") -%}
     {%- endif -%}
 
-{%- endfor -%}
 
-{# {{ concat(fields) }} #}
-{{ hash(concat(fields)) }}
+    {%- set fields = [] -%}
+
+    {%- for field in field_list_xf -%}
+
+        {%- set _ = fields.append(
+            "coalesce(cast(" ~ field ~ " as NVARCHAR " ~ "), '')"
+        ) -%}
+
+        {%- if not loop.last %}
+            {%- set _ = fields.append("'-'") -%}
+        {%- endif -%}
+
+    {%- endfor -%}
+
+    {# {{ concat(fields) }} #}
+    {{ mybi_dbt_core.hash(mybi_dbt_core.concat(fields)) }}
 
 {%- endmacro -%}
 
@@ -62,41 +61,40 @@
 --------------------------------
 {%- macro concat_key(field_list) -%}
 
+    {%- if varargs|length >= 1 %}
 
-{%- if varargs|length >= 1 %}
+    {%- do exceptions.warn("Warning: the `surrogate_key` macro now takes a single list argument instead of multiple string arguments. Support for multiple string arguments will be deprecated in a future release of dbt-utils.") -%}
 
-{%- do exceptions.warn("Warning: the `surrogate_key` macro now takes a single list argument instead of multiple string arguments. Support for multiple string arguments will be deprecated in a future release of dbt-utils.") -%}
+    {# first argument is not included in varargs, so add first element to field_list_xf #}
+    {%- set field_list_xf = [field_list] -%}
 
-{# first argument is not included in varargs, so add first element to field_list_xf #}
-{%- set field_list_xf = [field_list] -%}
+    {%- for field in varargs %}
+    {%- set _ = field_list_xf.append(field) -%}
+    {%- endfor -%}
 
-{%- for field in varargs %}
-{%- set _ = field_list_xf.append(field) -%}
-{%- endfor -%}
+    {%- else -%}
 
-{%- else -%}
+    {# if using list, just set field_list_xf as field_list #}
+    {%- set field_list_xf = field_list -%}
 
-{# if using list, just set field_list_xf as field_list #}
-{%- set field_list_xf = field_list -%}
-
-{%- endif -%}
-
-
-{%- set fields = [] -%}
-
-{%- for field in field_list_xf -%}
-
-    {%- set _ = fields.append(
-        "coalesce(cast(" ~ field ~ " as NVARCHAR " ~ "), '')"
-    ) -%}
-
-    {%- if not loop.last %}
-        {%- set _ = fields.append("'-'") -%}
     {%- endif -%}
 
-{%- endfor -%}
 
-{# {{ concat(fields) }} #}
-{{ concat(fields) }}
+    {%- set fields = [] -%}
+
+    {%- for field in field_list_xf -%}
+
+        {%- set _ = fields.append(
+            "coalesce(cast(" ~ field ~ " as NVARCHAR " ~ "), '')"
+        ) -%}
+
+        {%- if not loop.last %}
+            {%- set _ = fields.append("'-'") -%}
+        {%- endif -%}
+
+    {%- endfor -%}
+
+    {# {{ concat(fields) }} #}
+    {{ mybi_dbt_core.concat(fields) }}
 
 {%- endmacro -%}
