7d91900d529c85bdf7b3e408db29b9b646b08e81
Artemiy Kozyr <a.kozyr@wheely.com>
2021-04-15 10:48:37 +0300

    configuring ci: PR number

--- macros/clean_up.sql
+++ macros/clean_up.sql
@@ -1,7 +1,7 @@
 -- Delete objects from database after CI tests
 {% macro clean_up() %}
 
-    {%- if target.name in ['core_ci', 'ci'] -%}
+    {%- if target.name in ['ci'] -%}
 
         {% set schema = target.schema ~ '_' ~ env_var('GITHUB_PR_NUMBER') | trim %}
         {% do adapter.drop_schema(api.Relation.create(database=target.database, schema=schema)) %}
