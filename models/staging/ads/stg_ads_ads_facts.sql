466e31f887577d2cdb390ea1a1e1e3044bb10c50
Artemiy Kozyr <a.kozyr@wheely.com>
2022-10-03 21:55:19 +0300

    test CI job

--- models/staging/ads/stg_ads_ads_facts.sql
+++ models/staging/ads/stg_ads_ads_facts.sql
@@ -26,6 +26,7 @@ from {{ source('ads', 'ads_facts') }} as f
 	inner join {{ ref('stg_general_dates') }} as gd
 		on gd.id = f.dates_id
 
+
 {{ source_filter_rows(
     account_id=var('account_id_ads')
 ) }}
