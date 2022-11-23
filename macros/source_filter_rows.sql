06b1468c105d8344fca5ef18d0cfd02e84ae3199
Artemiy Kozyr <a.kozyr@wheely.com>
2022-11-23 15:48:27 +0300

    specify namespace explicitly for macro dispatch

--- macros/source_filter_rows.sql
+++ macros/source_filter_rows.sql
@@ -15,10 +15,10 @@
     
     {#- prepare expression to limit rows depending on target.name and flags provided -#}
     {%- if target.name in ['prod'] and history_depth_days > 0 -%}
-        {%- set watermark = watermark(history_depth_days) -%}
+        {%- set watermark = mybi_dbt_core.watermark(history_depth_days) -%}
         {%- set history_depth_expression = timestamp_column ~ ' >= ' ~ watermark -%}
     {%- elif target.name not in ['prod'] and limit_data_for_dev == true -%}
-        {%- set watermark = watermark(var('limit_data_days')) -%}
+        {%- set watermark = mybi_dbt_core.watermark(var('limit_data_days')) -%}
         {%- set history_depth_expression = timestamp_column ~ ' >= ' ~ watermark -%}
     {%- else -%}
         {%- set history_depth_expression = 'TRUE' -%}
@@ -33,7 +33,7 @@
 
 
 {% macro watermark(history_depth_days) %}
-    {{ return(adapter.dispatch('watermark') (history_depth_days)) }}
+    {{ return(adapter.dispatch('watermark', 'mybi_dbt_core') (history_depth_days)) }}
 {% endmacro %}
 
 {% macro default__watermark(history_depth_days) %}
